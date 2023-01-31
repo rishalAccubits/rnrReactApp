5d96447692710bfaa48b120a9d89f1f357f7b1c4
arun <arun.rs@accubits.com>
2023-01-31 20:00:57 +0530

    Update server url

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 // export const serverUrl = process.env.REACT_APP_EXTERNAL_URL
 
-export const serverUrl = 'https://0895-2405-201-f00c-f059-868e-6b54-1fd1-f797.in.ngrok.io'
+export const serverUrl = 'https://api-generic-crack-enigma.devtomaster.com'
 export const chainIdentifier = "5"
 export const events = {
     cryptopati:{ 
